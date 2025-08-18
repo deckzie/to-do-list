f0da1da329b915a5977384e4b3daf57806e0199b
deckzie <mnculaniban@up.edu.ph>
2025-08-18 15:23:02 +0800

    feat(models): add relationship between user and todo

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Todo extends Model
 { 
     use HasFactory;
+    protected $fillable = ['user_id', 'title', 'description', 'status'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
