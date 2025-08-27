107a1a5544cc4a4f51d8f3944618853361abf1c0
deckzie <mnculaniban@up.edu.ph>
2025-08-27 09:31:34 +0800

    fix(ui): make todo item responsive

--- vue/src/views/TodoList.vue
+++ vue/src/views/TodoList.vue
@@ -60,8 +60,6 @@
 
 
 <template>
-  
-
   <div class="max-w-md mx-auto mt-10 space-y-1">
     <h1 class="text-2xl font-bold text-center text-green-700 mb-6">TodoList</h1>
     <div
@@ -69,15 +67,16 @@
       :key="index"
       class="flex items-center justify-between bg-white shadow p-4 rounded-lg"
     >
-      <div class="flex items-center space-x-3">
+      <!-- Left side: checkbox + title -->
+      <div class="flex items-center space-x-3 flex-1 overflow-hidden">
         <input
           type="checkbox"
           :checked="todo.completed"
-           @change="toggleCompleted(todo)"
-          class="form-checkbox h-5 w-5 text-green-500"
+          @change="toggleCompleted(todo)"
+          class="form-checkbox h-5 w-5 text-green-500 flex-shrink-0"
         />
         <span
-          class="max-w-xs truncate"
+          class="truncate w-full"
           :class="{
             'line-through text-gray-400': todo.completed,
             'text-gray-800': !todo.completed
@@ -86,16 +85,19 @@
           {{ todo.title }}
         </span>
       </div>
-      <div class="flex space-x-2">
-        <button @click="editTodo(index)" class="text-grey-500 hover:text-blue-700" title="Edit task">
-           <PencilSquareIcon class="w-5 h-5" />
+
+      <!-- Right side: buttons -->
+      <div class="flex space-x-1 flex-shrink-0">
+        <button @click="editTodo(index)" class="text-gray-500 hover:text-gray-700" title="Edit task">
+          <PencilSquareIcon class="w-5 h-5" />
         </button>
-        <button @click="deleteTodo(index)" class="text-grey-500 hover:text-red-700" title="Delete task">
+        <button @click="deleteTodo(index)" class="text-gray-500 hover:text-gray-700" title="Delete task">
           <TrashIcon class="w-5 h-5" />
         </button>
       </div>
     </div>
   </div>
+
   <TodoForm
     v-if="showForm"
     :todo="selectedTodo"
