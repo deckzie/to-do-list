4df79a5d5dfd72554545b613d4944df86b510cd7
deckzie <mnculaniban@up.edu.ph>
2025-08-28 17:18:13 +0800

    chore: remove unused directory

--- frontend/src/lib/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
-  withCredentials: false, // set true if you add Sanctum later
-});
-
-export default api;
