d6f142053c70929c125c4d97b3217688927f932a
deckzie <mnculaniban@up.edu.ph>
2025-08-29 15:29:58 +0800

    style: padding on edit button

--- vue/src/views/TodoList.vue
+++ vue/src/views/TodoList.vue
@@ -166,7 +166,7 @@
 
       <!-- Right side: buttons -->
       <div class="flex space-x-1 flex-shrink-0" @click.stop>
-        <button @click="editTodo(index)" class="text-gray-500 hover:text-gray-700 cursor-pointer" title="Edit task">
+        <button @click="editTodo(index)" class="text-gray-500 hover:text-gray-700 cursor-pointer pl-2" title="Edit task">
           <PencilSquareIcon class="w-5 h-5" />
         </button>
         <button @click="deleteTodo(index)" class="text-gray-500 hover:text-gray-700 cursor-pointer" title="Delete task">
