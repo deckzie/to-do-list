0daadad3d4616f86c3e5f5beb4bf5ff99178e092
deckzie <mnculaniban@up.edu.ph>
2025-08-29 10:49:51 +0800

    fix

--- vue/src/axios.js
+++ vue/src/axios.js
@@ -4,6 +4,7 @@ import router from "./router";
 
 const axiosClient = axios.create({
     baseURL: "http://localhost:8000/api",
+    withCredentials: true,
     headers: {
         "Content-Type": "application/json"
     }
