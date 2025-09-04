e0942adb56989ef448aeccf543c4a28152e697ea
deckzie <mnculaniban@up.edu.ph>
2025-09-04 11:33:34 +0800

    fix seeeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             UsersTableSeeder::class,
-            TodosTableSeeder::class,
-            CategoriesTableSeeder::class
+            CategoriesSeeder::class,
+            TodosTableSeeder::class
         ]);
     }
 }
